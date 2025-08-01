75bed7d42c2d026c660f393b011c3d9cfdb985c4
Ivan Leonard <ivanleonardseo@yahoo.com>
2025-08-01 15:51:03 +0200

    folder structure and initial HTML

---
+++ server.js
@@ -0,0 +1,5 @@
+// Webhook simulation project using Node.js, Express, Socket.io
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
